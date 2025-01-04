765c8454f44b710a4e3e577f3a94c4aa4c123b44
Daniel Kowalski <danielk.biznes@gmail.com>
2025-01-04 20:52:48 +0100

    Add copying files to yolo dataset

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,14 +1,28 @@
 import os
+import shutil
+from pathlib import Path
 
 base_dir = "dataset"
 sub_dirs = [
     "train/images",
     "train/labels",
-    "val/images",
-    "val/labels"
+    "valid/images",
+    "valid/labels"
 ]
 
 for sub_dir in sub_dirs:
     directory = os.path.join(base_dir, sub_dir)
     if not os.path.exists(directory):
         os.makedirs(directory)
+        
+def copy_images_and_labels(category):
+    source_directory = f"PKLot/{category}/"
+    for image in Path(source_directory).glob("*.jpg"):
+        shutil.copy(image, f"{base_dir}/{category}/images")
+        label = image.with_suffix(".txt")
+        if not label.exists():
+            label.touch()
+        shutil.copy(label, f"{base_dir}/{category}/labels")
+
+copy_images_and_labels("train")
+copy_images_and_labels("valid")
