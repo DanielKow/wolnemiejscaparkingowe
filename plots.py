c2a192950cac4adf706d51bd80229b5ad3b654d9
Daniel Kowalski <danielk.biznes@gmail.com>
2025-01-04 20:36:08 +0100

    Add changing coco to yolo annotations

--- plots.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-def show_images(images):
-    for number, img in enumerate(images):
-        plt.subplot(1, len(images), number+1)
-        plt.imshow(img, cmap="gray")
-        plt.axis("off")
-    plt.show()
