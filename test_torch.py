5fa1cd1c77f6572c074cd273a97ab70e3c3e2178
Daniel Kowalski <danielk.biznes@gmail.com>
2025-01-05 13:56:10 +0100

    Update hardware checks, config, and training parameters
    Enhanced hardware availability checks in test_torch.py to include CUDA details. Updated parking_slots.yaml with new NMS time limit parameter. Simplified training configuration in train_model.py by removing unused batch size and tweaking confidence threshold.

--- test_torch.py
+++ test_torch.py
@@ -1,4 +1,13 @@
 import torch
 
-print(torch.__version__)  # Confirm PyTorch version
-print(torch.backends.mps.is_available())  
+print(torch.__version__)
+
+if (torch.backends.mps.is_available()):
+    print("MPS is available!")
+
+if torch.cuda.is_available():
+    print("CUDA is available!")
+    print(f"Number of GPUs: {torch.cuda.device_count()}")
+    print(f"Device Name: {torch.cuda.get_device_name(0)}")
+else:
+    print("CUDA is not available on this system.")
--- train_model.py
+++ train_model.py
@@ -1,4 +1,3 @@
-from click.core import batch
 from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')
@@ -7,7 +6,5 @@ model.train(
     epochs=50,
     imgsz=640,
     device="mps",
-    batch=8,
-    conf=0.3,         # Increase confidence threshold
-    max_det=200       # Decrease from the default (300)
+    conf=0.4
 )
