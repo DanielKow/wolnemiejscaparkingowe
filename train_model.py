d85709979f0ea43975090790f0cc44f90320113d
Daniel Kowalski <danielk.biznes@gmail.com>
2025-01-05 11:25:49 +0100

    Set batch size to 8 in YOLO model training.
    Added a batch size parameter to the training configuration in `train_model.py`. This change ensures more efficient utilization of resources during model training.

--- train_model.py
+++ train_model.py
@@ -1,4 +1,5 @@
+from click.core import batch
 from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')
-model.train(data='parking_slots.yaml', epochs=50, imgsz=640, device="mps")
+model.train(data='parking_slots.yaml', epochs=50, imgsz=640, device="mps", batch=8)
